1ebc79762f8cfe3df3ec3ec61b4685af47e2d6c8
Kelly Selden <kellyselden@gmail.com>
2017-06-19 11:01:07 -0700

    add ember-cli-deploy

--- config/environment.js
+++ config/environment.js
@@ -43,7 +43,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.rootURL = '/ember-vr-shopping';
+    ENV.locationType = 'hash';
   }
 
   return ENV;
