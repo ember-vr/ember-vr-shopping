a8812eca5b16f87c1175ccc8527ab95bcb962168
Kelly Selden <kellyselden@gmail.com>
2017-06-28 20:06:30 -0700

    sync camera move with query params

--- app/controllers/vr.js
+++ app/controllers/vr.js
@@ -1,4 +1,8 @@
 import Controller from 'ember-controller';
 import Shared from '../mixins/shared-controller';
+import QueryParamsControllerMixin from 'ember-aframe-camera-extras/mixins/query-params-controller';
 
-export default Controller.extend(Shared);
+export default Controller.extend(
+  Shared,
+  QueryParamsControllerMixin
+);
