eec158eb3794b504f42426da5d93d21dd14d84a2
Kelly Selden <kellyselden@gmail.com>
2017-07-16 21:35:54 -0700

    positionalParams cameraQueryParams

--- app/components/vr/camera-component.js
+++ app/components/vr/camera-component.js
@@ -1,10 +1,16 @@
 import ACamera from 'ember-aframe/components/a-camera';
 import ComponentActionsMixin from 'ember-aframe-camera-extras/mixins/component-actions';
 
-export default ACamera.extend(ComponentActionsMixin, {
+const MyComponent = ACamera.extend(ComponentActionsMixin, {
   attributeBindings: [
     'mouse-cursor'
   ],
 
   'mouse-cursor': true
 });
+
+MyComponent.reopenClass({
+  positionalParams: ['cameraQueryParams']
+});
+
+export default MyComponent;
