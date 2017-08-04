905e52b10e3975790b92fbf016a20eabe74d15de
Kelly Selden <kellyselden@gmail.com>
2017-08-03 20:01:36 -0700

    ember cli 2.14.2

--- config/environment.js
+++ config/environment.js
@@ -1,9 +1,10 @@
 /* eslint-env node */
+'use strict';
 
 module.exports = function(environment) {
-  var ENV = {
+  let ENV = {
     modulePrefix: 'ember-vr-shopping',
-    environment: environment,
+    environment,
     rootURL: '/',
     locationType: 'auto',
     EmberENV: {
