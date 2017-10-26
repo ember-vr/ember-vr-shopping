8571f8042a6416dca782ae9667913588ccf566ea
Kelly Selden <kellyselden@gmail.com>
2017-10-25 19:25:48 -0700

    update ember-cli 2.16.2

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-import Ember from 'ember';
+import Application from '@ember/application';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
@@ -6,7 +6,7 @@ import 'aframe';
 import 'npm:aframe-layout-component';
 import 'npm:aframe-mouse-cursor-component';
 
-const App = Ember.Application.extend({
+const App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver
