3ba79c2fdd7906b041a3e85eddb59568538cd5e4
Kelly Selden <kellyselden@gmail.com>
2018-10-27 10:16:22 +0100

    ember-cli 3.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,8 +26,8 @@ module.exports = {
     // node files
     {
       files: [
-        'testem.js',
         'ember-cli-build.js',
+        'testem.js',
         'config/**/*.js',
         'lib/*/index.js'
       ],
@@ -38,15 +38,6 @@ module.exports = {
       env: {
         browser: false,
         node: true
-      }
-    },
-
-    // test files
-    {
-      files: ['tests/**/*.js'],
-      excludedFiles: ['tests/dummy/**/*.js'],
-      env: {
-        embertest: true
       }
     }
   ]
