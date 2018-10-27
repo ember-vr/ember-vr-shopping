020357a77628c7ecab8bf4be3c1422e82b48c9b3
Kelly Selden <kellyselden@gmail.com>
2018-10-27 20:31:34 +0100

    ember-cli 3.1

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,6 +28,7 @@ module.exports = {
       files: [
         'ember-cli-build.js',
         'testem.js',
+        'blueprints/*/index.js',
         'config/**/*.js',
         'lib/*/index.js'
       ],
