b455bc08293349739ee4130363b2fc6ed3f45bef
Kelly Selden <kellyselden@gmail.com>
2018-10-27 11:22:10 +0100

    remove browserify

--- app/app.js
+++ app/app.js
@@ -2,9 +2,6 @@ import Application from '@ember/application';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
-import 'aframe';
-import 'npm:aframe-layout-component';
-import 'npm:aframe-mouse-cursor-component';
 
 const App = Application.extend({
   modulePrefix: config.modulePrefix,
