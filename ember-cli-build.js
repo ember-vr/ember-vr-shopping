8c712fccdac0d1a633040e3540931d13ad3c0781
Kelly Selden <kellyselden@gmail.com>
2019-11-06 13:29:10 +0000

    update ember-cli-mirage

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -22,6 +22,12 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import('node_modules/faker/build/build/faker.js', {
+    using: [
+      { transformation: 'amd', as: 'faker' }
+    ]
+  });
+
   app.import('node_modules/aframe-layout-component/dist/aframe-layout-component.js');
 
   return app.toTree();
