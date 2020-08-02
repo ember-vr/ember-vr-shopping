963e2e9013c16c43f462541737f1fb959a81413e
Kelly Selden <kellyselden@gmail.com>
2020-08-02 19:32:14 +0100

    add comment

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,3 +1,5 @@
+// This is only needed to satisfy the `ember-data:default-serializer` deprecation
+// because we have a serializer in ember-cli-mirage too.
 import JSONAPISerializer from '@ember-data/serializer/json-api';
 
 export default JSONAPISerializer.extend({
