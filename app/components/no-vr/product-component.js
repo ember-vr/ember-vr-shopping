68dd15a0625462d75f95e2573f5469b3fb1fc0c9
Kelly Selden <kellyselden@gmail.com>
2020-08-02 15:58:08 +0100

    add a basic acceptance test

--- app/components/no-vr/product-component.js
+++ app/components/no-vr/product-component.js
@@ -1,6 +1,8 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  'data-test-product-component': '',
+
   classNames: ['product-component']
 }).reopenClass({
   positionalParams: ['product']
