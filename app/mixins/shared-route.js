31074e0ea7392ab989b813b1e15cb1b104476a81
Kelly Selden <kellyselden@gmail.com>
2020-09-20 12:04:48 +0100

    fix new lint errors

--- app/mixins/shared-route.js
+++ app/mixins/shared-route.js
@@ -1,7 +1,6 @@
 /* eslint-disable ember/no-new-mixins */
 import Mixin from '@ember/object/mixin';
 import { hash } from 'rsvp';
-import { get } from '@ember/object';
 
 export default Mixin.create({
   model() {
@@ -13,11 +12,11 @@ export default Mixin.create({
 
   actions: {
     addToCart(cart, product) {
-      get(cart, 'products').pushObject(product);
+      cart.products.pushObject(product);
       cart.save();
     },
     removeFromCart(cart, product) {
-      get(cart, 'products').removeObject(product);
+      cart.products.removeObject(product);
       cart.save();
     }
   }
