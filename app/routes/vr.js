5f82a73e2a42f3ea6cac30ec539334c752514a83
Kelly Selden <kellyselden@gmail.com>
2020-09-20 10:47:42 +0100

    fix new lint errors

--- app/routes/vr.js
+++ app/routes/vr.js
@@ -1,5 +1,7 @@
 import Route from '@ember/routing/route';
+// eslint-disable-next-line ember/no-mixins
 import Shared from '../mixins/shared-route';
+// eslint-disable-next-line ember/no-mixins
 import QueryParamsRouteMixin from 'ember-aframe-camera-extras/mixins/query-params-route';
 
 export default Route.extend(
